fe68cfd4110ce6234349df02a9ce36bf8f32ddfe
rajeshwari114v <rajeshwari114v@gmail.com>
2024-08-14 23:38:26 +0530

    ExceptionDemo

---
+++ DemoProject/src/exception/MainDemo1.java
@@ -0,0 +1,14 @@
+package DemoProject.src.exception;
+
+public class MainDemo1 {
+    public static void main(String[] args) {
+
+        for(int i=20;i<=40;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println("Even Number :"+i);
+            }
+        }
+    }
+}
