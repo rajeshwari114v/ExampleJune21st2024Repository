51a412197746f574d933119495d7c4e3f6db92de
rajeshwari114v <rajeshwari114v@gmail.com>
2024-08-18 00:02:01 +0530

    EXCEL

---
+++ EXcelAutomation/src/com/sgtesting/java/ReadExcelFileContent.java
@@ -0,0 +1,62 @@
+package com.sgtesting.java;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+
+public class ReadExcelFileContent {
+    public static void main(String[] args) {
+        readContent();
+    }
+
+    private static void readContent()
+    {
+        FileInputStream fin=null;
+        Workbook wb=null;
+        Sheet sh=null;
+        Row row=null;
+        Cell cell=null;
+        try
+        {
+            fin=new FileInputStream("C:\\EXCEL\\Test.xlsx");
+            wb=new XSSFWorkbook(fin);
+
+            sh=wb.getSheet("Sheet1");
+
+            int rc=sh.getPhysicalNumberOfRows();
+            for(int r=0;r<rc;r++)
+            {
+                row=sh.getRow(r);
+                int cc=row.getPhysicalNumberOfCells();
+                for(int c=0;c<cc;c++)
+                {
+                    cell=row.getCell(c);
+                    String data=cell.getStringCellValue();
+                    System.out.print(data+"  ");
+                }
+                System.out.println();
+            }
+
+
+
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                fin.close();
+                wb.close();
+            }catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
